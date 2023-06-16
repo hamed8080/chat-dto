a08372d9663303b13aba120997905a4c89e25fe9
Hamed Hosseini <hamed8080@gmail.com>
2023-06-16 12:23:00 +0330

    - Fix blockId - Add BlockedContactResponse

--- Sources/ChatDTO/Contact/DTO/REQ/UnBlockRequest.swift
+++ Sources/ChatDTO/Contact/DTO/REQ/UnBlockRequest.swift
@@ -7,16 +7,19 @@
 import Foundation
 
 public struct UnBlockRequest: Encodable, UniqueIdProtocol {
+    public let blockId: Int?
     public let contactId: Int?
     public let threadId: Int?
     public let userId: Int?
     public let uniqueId: String
 
-    public init(contactId: Int? = nil,
+    public init(blockId: Int? = nil,
+                contactId: Int? = nil,
                 threadId: Int? = nil,
                 userId: Int? = nil,
                 uniqueId: String = "G-\(UUID().uuidString)")
     {
+        self.blockId = blockId
         self.contactId = contactId
         self.threadId = threadId
         self.userId = userId
@@ -27,10 +30,12 @@ public struct UnBlockRequest: Encodable, UniqueIdProtocol {
         case contactId
         case threadId
         case userId
+        case blockId
     }
 
     public func encode(to encoder: Encoder) throws {
         var container = encoder.container(keyedBy: CodingKeys.self)
+        try? container.encodeIfPresent(blockId, forKey: .blockId)
         try? container.encodeIfPresent(contactId, forKey: .contactId)
         try? container.encodeIfPresent(threadId, forKey: .threadId)
         try? container.encodeIfPresent(userId, forKey: .userId)
