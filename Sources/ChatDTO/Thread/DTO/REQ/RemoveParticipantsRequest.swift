f39f000d8b9fe399c6a37faa504acedf5af7ab31
Hamed Hosseini <hamed8080@gmail.com>
2023-07-03 08:33:53 +0330

    - Add ability to Remove Participants By Invitee list in RemoveParticipantsRequest

--- Sources/ChatDTO/Thread/DTO/REQ/RemoveParticipantsRequest.swift
+++ Sources/ChatDTO/Thread/DTO/REQ/RemoveParticipantsRequest.swift
@@ -5,9 +5,11 @@
 // Created by Hamed Hosseini on 12/14/22
 
 import Foundation
+import ChatModels
 
 public struct RemoveParticipantsRequest: Encodable, UniqueIdProtocol {
-    public let participantIds: [Int]
+    public let participantIds: [Int]?
+    public let invitees: [Invitee]?
     public let threadId: Int
     public let uniqueId: String
 
@@ -15,24 +17,35 @@ public struct RemoveParticipantsRequest: Encodable, UniqueIdProtocol {
         self.threadId = threadId
         participantIds = [participantId]
         self.uniqueId = UUID().uuidString
+        invitees = nil
     }
 
     public init(participantIds: [Int], threadId: Int) {
         self.threadId = threadId
         self.participantIds = participantIds
         self.uniqueId = UUID().uuidString
+        invitees = nil
+    }
+
+    public init(invitess: [Invitee], threadId: Int) {
+        self.threadId = threadId
+        self.invitees = invitess
+        self.uniqueId = UUID().uuidString
+        participantIds = nil
     }
 
     private enum CodingKeys: CodingKey {
         case participantIds
+        case invitees
         case threadId
         case uniqueId
     }
 
     public func encode(to encoder: Encoder) throws {
         var container = encoder.container(keyedBy: CodingKeys.self)
-        try container.encode(self.participantIds, forKey: .participantIds)
-        try container.encode(self.threadId, forKey: .threadId)
-        try container.encode(self.uniqueId, forKey: .uniqueId)
+        try container.encodeIfPresent(participantIds, forKey: .participantIds)
+        try container.encodeIfPresent(invitees, forKey: .invitees)
+        try container.encode(threadId, forKey: .threadId)
+        try container.encode(uniqueId, forKey: .uniqueId)
     }
 }
