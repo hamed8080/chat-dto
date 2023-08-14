daac2b3b9483193b2e085d6211e00c785f17fd84
Hamed Hosseini <hamed8080@gmail.com>
2023-08-14 10:05:43 +0330

    - Fix remove reaction DeleteReactionRequest.swift

--- Sources/ChatDTO/Reaction/DTO/REQ/DeleteReactionRequest.swift
+++ Sources/ChatDTO/Reaction/DTO/REQ/DeleteReactionRequest.swift
@@ -8,10 +8,21 @@ import Foundation
 
 public struct DeleteReactionRequest: Encodable, UniqueIdProtocol {
     public let reactionId: Int
+    public let conversationId: Int
     public let uniqueId: String
 
-    public init(reactionId: Int) {
+    public init(reactionId: Int, conversationId: Int) {
         self.reactionId = reactionId
+        self.conversationId = conversationId
         self.uniqueId = UUID().uuidString
+    }
+
+    private enum CodingKeys: CodingKey {
+        case reactionId
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(self.reactionId, forKey: .reactionId)
     }
 }
