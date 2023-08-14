707c7f7cc143d7341cb3956e13fb1938599ebdf3
Hamed Hosseini <hamed8080@gmail.com>
2023-08-14 12:33:14 +0330

    - Add reaction count request(BETA)

---
+++ Sources/ChatDTO/Reaction/DTO/REQ/RactionCountRequest.swift
@@ -0,0 +1,19 @@
+//
+// RactionCountRequest.swift
+// Copyright (c) 2022 ChatDTO
+//
+// Created by Hamed Hosseini on 12/14/22
+
+import Foundation
+
+public struct RactionCountRequest: Encodable, UniqueIdProtocol {
+    public let messageIds: [Int]
+    public let conversationId: Int
+    public let uniqueId: String
+
+    public init(messageIds: [Int], conversationId: Int) {
+        self.messageIds = messageIds
+        self.conversationId = conversationId
+        self.uniqueId = UUID().uuidString
+    }
+}
