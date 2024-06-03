hamed8080/chat-extensions

Merge main into develop

Package.swift
@@ -17,27 +17,27 @@ let package = Package(
             targets: ["ChatExtensions"]),
     ],
     dependencies: [
-        .package(url: "https://pubgi.sandpod.ir/chat/ios/chat-transceiver", from: "2.0.1"),
-        .package(url: "https://pubgi.sandpod.ir/chat/ios/chat-cache", from: "2.0.1"),
-        .package(url: "https://pubgi.sandpod.ir/chat/ios/chat-core", from: "2.0.1"),
+        .package(path: "../ChatTransceiver"),
+        .package(path: "../ChatCache"),
+        .package(path: "../ChatCore"),
         .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
     ],
     targets: [
         .target(
             name: "ChatExtensions",
             dependencies: [
-                .product(name: "ChatTransceiver", package: "chat-transceiver"),
-                .product(name: "ChatCache", package: "chat-cache"),
-                .product(name: "ChatCore", package: "chat-core"),
+                .product(name: "ChatTransceiver", package: "ChatTransceiver"),
+                .product(name: "ChatCache", package: "ChatCache"),
+                .product(name: "ChatCore", package: "ChatCore"),
             ]
         ),
         .testTarget(
             name: "ChatExtensionsTests",
             dependencies: [
                 "ChatExtensions",
-                .product(name: "ChatTransceiver", package: "chat-transceiver"),
-                .product(name: "ChatCache", package: "chat-cache"),
-                .product(name: "ChatCore", package: "chat-core"),
+                .product(name: "ChatTransceiver", package: "ChatTransceiver"),
+                .product(name: "ChatCache", package: "ChatCache"),
+                .product(name: "ChatCore", package: "ChatCore"),
             ],
             resources: []
         ),

hamed8080/chat-dto

Package.swift
@@ -17,20 +17,20 @@ let package = Package(
             targets: ["ChatDTO"]),
     ],
     dependencies: [
-        .package(url: "https://pubgi.sandpod.ir/chat/ios/chat-models", from: "2.0.1"),
+        .package(path: "../ChatModels"),
         .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
     ],
     targets: [
         .target(
             name: "ChatDTO",
             dependencies: [
-                .product(name: "ChatModels", package: "chat-models"),
+                .product(name: "ChatModels", package: "ChatModels"),
             ]
         ),
         .testTarget(
             name: "ChatDTOTests",
             dependencies: [
-                .product(name: "ChatModels", package: "chat-models"),
+                .product(name: "ChatModels", package: "ChatModels"),
             ],
             resources: []
         ),